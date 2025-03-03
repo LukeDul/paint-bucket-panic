0a54957bd7b7e4ccff5da7fdb2bfea0842aa0de2
Dulworth, Luke <44180963+LukeDul@users.noreply.github.com>
2025-03-03 13:11:07 -0600

    some refactoring

--- luke_main.gd
+++ luke_main.gd
@@ -1,46 +1,54 @@
 extends Node2D
-var player
-var last_height 
-var random_step
+
+const PLATFORM_Y_STEP = 40
+const PLATFORM_Y_LOWER_BOUND = 10
+const PLATFORM_Y_UPPER_BOUND = 20
+const PLATFORM_X_LOWER_BOUND = -15
+const PLATFORM_X_UPPER_BOUND = 15
+
+var last_height # tracks the players last height 
+
+@onready var player = $Player
+@onready var random_step = calc_random_step()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player = get_node("Player")
 	last_height = player.position.y
-	random_step = calc_random_step()
-	PhysicsServer2D.set_active(true)
+	
 	$AudioStreamPlayer2DMusic.play()	
-func calc_random_step()->int: return randi_range(10, 20) 
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if player.position.y < last_height - random_step:
-		_gen_new_platforms(randi_range(-15, 15), player.position.y - 40)
+	if player.position.y < last_height - random_step: # if the player is higher than he was last frame + [10-20]
+		_gen_new_platform(randi_range(PLATFORM_X_LOWER_BOUND, PLATFORM_X_UPPER_BOUND), player.position.y - PLATFORM_Y_STEP) 
+		
 		last_height = player.position.y
 		random_step = calc_random_step()
-		
-# Add at the top of your platform generator script
-var power_up_scene = preload("res://PowerUp.tscn")  # Update this path
 
-# In your _gen_new_platforms function
-func _gen_new_platforms(x: float, y: int) -> void:
+
+func calc_random_step()->int: 
+	"""Returns a value between PLATFORM_Y_LOWER_BOUND and PLATFORM_Y_UPPER_BOUND"""
+	return randi_range(PLATFORM_Y_LOWER_BOUND, PLATFORM_Y_UPPER_BOUND) 
+
+
+func _gen_new_platform(x: float, y: int) -> void:
+	"""Places a platform of random type at the coords"""
 	var platform_type = randi() % 2  # Randomly choose 0 or 1
 	var scene_path = ""
 	
-	if platform_type == 0:
-		scene_path = "res://static platform.tscn"
-	else:
-		scene_path = "res://moving_platform.tscn"
+	if platform_type == 0: scene_path = "res://static platform.tscn"
+	else: scene_path = "res://moving_platform.tscn"
 	
-	var scene = load(scene_path)  # Load the selected scene
-	var instance = scene.instantiate()  # Create an instance
-	instance.position = Vector2(x, y)  # Set the position
-	add_child(instance)  # Add it to the current node
+	var scene = load(scene_path) 
+	var instance = scene.instantiate() 
+	instance.position = Vector2(x, y)  
+	self.add_child(instance) 
 	
 	# Spawn power-up with a 10% chance (increased for testing)
 	if randf() < 0.1:
-		print("Attempting to spawn power-up")
+		var power_up_scene = preload("res://PowerUp.tscn") 
 		var power_up = power_up_scene.instantiate()
 		power_up.position = Vector2(x, y - 30)  # Position above platform
 		add_child(power_up)
-		print("Power-up spawned successfully")
--- paint.gd
+++ paint.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 var initial_speed = 3.0        # Increased starting speed (was 1.0)
 var acceleration = 3.0        # Increased acceleration rate (was 1.0)
-var max_speed = 60.0         # Increased maximum speed (was 20.0)
+var max_speed = 20.0         # Increased maximum speed (was 20.0)
 var current_speed = 0.0
 var paint_height = 20.0       # Current height in pixels (grows upward)
 var distance_multiplier = 0.0 # Additional speed based on distance from player
@@ -18,40 +18,36 @@ var is_sound_playing = false   # To prevent multiple sound instances
 @onready var player = null     # Will store reference to player
 @onready var sound_player = $AudioStreamPlayer2DBlurbNoise  # Add this line - need to add node in scene
 
+
 func _ready():
 	current_speed = initial_speed
-	player = get_node("/root/Main/Player")  # Adjust this path to match your scene structure
-	
-	# Set initial distance
-	if player:
-		var paint_top = global_position.y - paint_height
-		var player_bottom = player.global_position.y
-		previous_distance = player_bottom - paint_top
+
 
 func _process(delta):
 	# Calculate distance-based multiplier and current distance
 	var current_distance = 0.0
-	if player:
-		var paint_top = global_position.y - paint_height
-		var player_bottom = player.global_position.y
-		current_distance = player_bottom - paint_top
-		
-		# Sound logic
-		if current_distance < sound_threshold and current_distance < previous_distance:
-			if not is_sound_playing and sound_player:
-				sound_player.play()
-				is_sound_playing = true
-		elif current_distance >= sound_threshold and is_sound_playing:
-			sound_player.stop()
-			is_sound_playing = false
-			
-		# Update previous distance
-		previous_distance = current_distance
-		
-		# More aggressive distance multiplier
-		distance_multiplier = clamp(current_distance / 5.0, 1.0, 8.0)
-	else:
-		distance_multiplier = 1.0
+	#if player:
+		#var paint_top = global_position.y - paint_height
+		#var player_bottom = player.global_position.y
+		#current_distance = player_bottom - paint_top
+		#
+		## Sound logic
+		#if current_distance < sound_threshold and current_distance < previous_distance:
+			#if not is_sound_playing and sound_player:
+				#sound_player.play()
+				#is_sound_playing = true
+		#elif current_distance >= sound_threshold and is_sound_playing:
+			#sound_player.stop()
+			#is_sound_playing = false
+			#
+		## Update previous distance
+		#previous_distance = current_distance
+		#
+		## More aggressive distance multiplier
+		#distance_multiplier = clamp(current_distance / 5.0, 1.0, 8.0)
+	#else:
+	
+	distance_multiplier = 1.0
 	
 	# Accelerate the growth rate
 	current_speed = min(current_speed + acceleration * delta, max_speed)
@@ -67,6 +63,7 @@ func _process(delta):
 	sprite.position.y = -paint_height / 2
 	face.position.y = -paint_height
 	collision_shape.position.y = face.position.y -4
+
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.name == 'Player':
